f4125f560425de3943b290216453f8ff5337a905
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-01-03 16:11:36 +0100

    export guards

--- src/sync.rs
+++ src/sync.rs
@@ -1,8 +1,8 @@
 #[cfg(not(feature = "std"))]
-pub use spin::{Lazy, Mutex, RwLock};
+pub use spin::{Lazy, Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 #[cfg(feature = "std")]
 pub use ::{
     once_cell::sync::Lazy,
-    parking_lot::{Mutex, RwLock},
+    parking_lot::{Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard},
 };
