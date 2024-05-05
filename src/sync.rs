30ac80b2e952851f58128f4032a3ee726029c76a
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2024-05-05 14:07:38 +0200

    once

--- src/sync.rs
+++ src/sync.rs
@@ -1,8 +1,9 @@
 #[cfg(not(feature = "std"))]
-pub use spin::{Lazy, Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard};
+pub use spin::{Lazy, Mutex, MutexGuard, Once, RwLock, RwLockReadGuard, RwLockWriteGuard};
 
 #[cfg(feature = "std")]
 pub use ::{
     once_cell::sync::Lazy,
     parking_lot::{Mutex, MutexGuard, RwLock, RwLockReadGuard, RwLockWriteGuard},
+    std::sync::Once,
 };
